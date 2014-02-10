cfccd1cb4337693db2aba3fb957baacc0d9f1af3
reinermeister <enneth.19@gmail.com>
2014-02-10 13:34:09 +0800

    Added BRCYPT Demos
    Added Demo for BCRYPT

---
+++ Encryption.aspx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using BCryptEncryption;
+
+public partial class Encryption : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        string salt = BCrypt.GenerateSalt(12);
+
+        string hashedstring = BCrypt.HashPassword("12345", salt);
+
+        Response.Write(hashedstring);
+
+        //bool isGood = BCrypt.CheckPassword("12345", hashedstring);
+
+        //if (isGood)
+        //{
+        //    Response.Write("Match!");
+        //}
+        //else
+        //{
+        //    Response.Write("Mismatch!");
+        //}
+
+
+
+    }
+}
